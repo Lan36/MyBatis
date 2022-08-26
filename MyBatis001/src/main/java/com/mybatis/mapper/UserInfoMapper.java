7f086debe1057c430939c55df111c5075d07929a
Lan <2142394426@qq.com>
2022-08-26 17:18:55 +0800

    hot-fix mybatis001

--- MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
+++ MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class UserInfoMapper {
     public static void main(String[] args) {
         System.out.println("怎么会是呢？");
+        System.out.println("怎么会是呢？");
     }
 }
