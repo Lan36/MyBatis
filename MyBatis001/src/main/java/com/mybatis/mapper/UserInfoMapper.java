283c2137031a88f23554a1d91b964683f18a06cb
Lan <2142394426@qq.com>
2022-08-26 17:13:12 +0800

    mybatis0012

--- MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
+++ MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
@@ -1,4 +1,9 @@
 package com.mybatis.mapper;
 
+import java.sql.SQLOutput;
+
 public class UserInfoMapper {
+    public static void main(String[] args) {
+        System.out.println("怎么会是呢？");
+    }
 }
