928a40ca51941d494573b3ae6d335fef1e448fb8
Lan <2142394426@qq.com>
2022-08-26 17:22:05 +0800

    master mybatis001

--- MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
+++ MyBatis001/src/main/java/com/mybatis/mapper/UserInfoMapper.java
@@ -6,5 +6,6 @@ public class UserInfoMapper {
     public static void main(String[] args) {
         System.out.println("怎么会是呢？");
         System.out.println("怎么会是呢？");
+        System.out.println("asddasda");
     }
 }
